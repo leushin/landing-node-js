729d5b2aed1427e3e52f9cda42c602a91fba0f10
Kesha <kesha.leushin@gmail.com>
2019-03-21 21:32:03 +0300

    first step

--- app.js
+++ app.js
@@ -1,64 +1,60 @@
+const Koa = require('koa');
+const _static = require('koa-static');
+const session = require('koa-session');
+const Pug = require('koa-pug');
+
 const createError = require('http-errors');
-const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const cookieSession = require('cookie-session');
-const helmet = require('helmet');
-const cluster = require('cluster');
+
+const app = new Koa();
+
+app.use(_static(path.join(__dirname, 'public')));
 
 require('./database');
 require('./engine');
+
 const rest = require('./rest');
 
-const app = express();
+const pug = new Pug({
+  viewPath: './views/pages',
+  pretty: false,
+  basedir: './views',
+  noCache: true,
+  app: app
+});
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views', 'pages'));
-app.set('view engine', 'pug');
-
-app.use((req, res, next) => {
-  if (cluster.isWorker) {
-    console.log('ID: ', cluster.worker.id);
+app.use(async (ctx, next) => {
+  try {
+    await next();
+    if (ctx.status === 404) {
+      let err = {
+        status: ctx.response.status,
+        error: ctx.response.message
+      };
+      ctx.app.emit('error', err, ctx);
+    }
+  } catch (err) {
+    ctx.status = err.status || 500;
+    ctx.body = err.message;
+    console.log(err);
+    ctx.app.emit('error', err, ctx);
   }
-  next();
 });
 
-app.use(logger('dev'));
-app.use(helmet());
-app.use(express.json({ limit: '2048kb' }));
-app.use(express.urlencoded({ extended: false, limit: 10000 }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-const expiryDate = new Date( Date.now() + 60 * 60 * 1000 ); // 1 hour
-app.use(cookieSession({
-  name: 'session',
-  keys: ['key1', 'key2'],
-  cookie: {
-    secure: false,
-    httpOnly: true,
-    expires: expiryDate
-  }
-})
-);
-
-app.use('/', rest);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.on('error', (err, ctx) => {
+  console.log('error', err)
+  ctx.render('error', {
+    status: ctx.response.status,
+    error: err
+  });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+//app.use(cookieParser());
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use(rest.routes());
+
+app.listen(3000, () => {
+  console.log('Server start on port: 3000');
 });
-
-module.exports = app;
--- rest/index.js
+++ rest/index.js
@@ -1,16 +1,9 @@
-const express = require('express');
+const Router = require('koa-router');
+const koaBody = require('koa-body');
+
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const rateLimit = require('express-rate-limit');
-
-const createAccountLimiter = rateLimit({
-  windowMs: 3 * 60 * 1000,
-  max: 3,
-  handler: (req, res) => {
-    res.json({ status: 'Превышен лимит попыток авторизаций. Попробуйте через 3 минуты' });
-  },
-});
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -22,59 +15,59 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
-const router = express.Router();
+const router = new Router();
 const ENGINE = global.ENGINE;
 
-router.get('/', (req, res) => {
-  ENGINE.emit('index/get')
-    .then(data => res.render('index', data))
-    .catch(error => res.render('error', { message: error.message }));
+router.get('/', async (ctx) => {
+  await ENGINE.emit('index/get')
+    .then(data => ctx.render('index', data))
+    .catch(error => ctx.render('error', { message: error.message }));
 });
 
 router.get('/login', (req, res) => {
-  ENGINE.emit('login/get')
-    .then(data => req.session.isAuth ? res.redirect('/admin') : res.render('login', data))
-    .catch(error => res.render('error', { message: error.message }));
-});
-
-router.get('/admin', (req, res) => {
-  ENGINE.emit('admin/get')
-    .then(data => req.session.isAuth ? res.render('admin', data) : res.redirect('/login'))
-    .catch(error => res.render('error', { message: error.message }));
-});
-
-router.post('/', (req, res) => {
-  ENGINE.emit('index/post', req.body)
-    .then(data => res.json(data))
-    .catch(error => res.json({ message: error.message }));
-});
-
-router.post('/login', (req, res) => {
-  ENGINE.emit('login/post', req.body)
-    .then(data => {
-      if (data.isAuth) {
-        req.session.isAuth = true;
-        return res.redirect('/admin');
-      } else {
-        return res.render('login', data);
-      }
-    })
-    .catch(error => res.render('error', { message: error.message }));
-});
-
-router.post('/admin/skills', (req, res) => {
-  ENGINE.emit('skills/add', req.body)
-    .then(() => res.redirect('/admin'))
-    .catch(error => res.render('error', { message: error.message }));
-});
-
-router.post('/admin/products', upload.single('photo'), (req, res) => {
-  ENGINE.emit('products/add', { body: req.body, file: req.file })
-    .then(() => res.redirect('/admin'))
-    .catch(error => {
-      fs.unlinkSync(req.file.path);
-      return res.render('error', { message: error.message });
-    });
-});
+    await ENGINE.emit('login/get')
+      .then(data => req.session.isAuth ? res.redirect('/admin') : res.render('login', data))
+      .catch(error => res.render('error', { message: error.message }));
+  });
+  
+  router.get('/admin', (req, res) => {
+    await ENGINE.emit('admin/get')
+      .then(data => req.session.isAuth ? res.render('admin', data) : res.redirect('/login'))
+      .catch(error => res.render('error', { message: error.message }));
+  });
+  
+  router.post('/', (req, res) => {
+    await ENGINE.emit('index/post', req.body)
+      .then(data => res.json(data))
+      .catch(error => res.json({ message: error.message }));
+  });
+  
+  router.post('/login', (req, res) => {
+    await ENGINE.emit('login/post', req.body)
+      .then(data => {
+        if (data.isAuth) {
+          req.session.isAuth = true;
+          return res.redirect('/admin');
+        } else {
+          return res.render('login', data);
+        }
+      })
+      .catch(error => res.render('error', { message: error.message }));
+  });
+  
+  router.post('/admin/skills', (req, res) => {
+    await ENGINE.emit('skills/add', req.body)
+      .then(() => res.redirect('/admin'))
+      .catch(error => res.render('error', { message: error.message }));
+  });
+  
+  router.post('/admin/products', upload.single('photo'), (req, res) => {
+    await ENGINE.emit('products/add', { body: req.body, file: req.file })
+      .then(() => res.redirect('/admin'))
+      .catch(error => {
+        fs.unlinkSync(req.file.path);
+        return res.render('error', { message: error.message });
+      });
+  });
 
 module.exports = router;
